6bafc54726db5359d9b6c89cacce5aa667c7158d
BrunoPaoloFigueroa <bruno.figueroa@usil.pe>
2025-05-02 03:41:54 -0500

    v2

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,28 @@
 #include <avr/interrupt.h>
 #define F_CPU 16000000UL
 #include <util/delay.h>
+char pos=0x00;
+
+ISR(INT1_vect) {
+    _delay_ms(50); 
+
+    switch (pos) {
+        case 1:
+            _delay_ms(500);
+            PORTD |= 0x80; 
+            pos = 2;
+            break;
+        case 2:
+            _delay_ms(500);
+            PORTD &= ~(0x80); 
+            _delay_ms(500);
+            PORTD |= 0x40; 
+            pos = 3;
+            break;
+        case 3:
+            _delay_ms(500);
+            PORTD &= ~(0x40); 
+            pos = 1;
+            break;
+    }
+}
